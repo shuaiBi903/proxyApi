385d149356b4da185d8f6745577caec80e9d1ecd
Syz9031 <105356480+shuaiBi903@users.noreply.github.com>
2024-01-07 00:01:32 +0800

    切换模式

--- _worker.js
+++ _worker.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const {
-  createProxyMiddleware
-} = require('http-proxy-middleware');
-const app = express()
-const port = 9000
+const TELEGRAPH_URL = 'https://api.openai.com';
 
-app.use('/', createProxyMiddleware({
-  target: 'https://api.openai.com',
-  changeOrigin: true,
-  onProxyReq: (proxyReq, req, res) => {
-    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
-    proxyReq.removeHeader('x-forwarded-for');
-    proxyReq.removeHeader('x-real-ip');
-  },
-  onProxyRes: function (proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-}));
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
 })
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const headers_Origin = request.headers.get("Access-Control-Allow-Origin") || "*"
+  url.host = TELEGRAPH_URL.replace(/^https?:\/\//, '');
+  const modifiedRequest = new Request(url.toString(), {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+    redirect: 'follow'
+  });
+  const response = await fetch(modifiedRequest);
+  const modifiedResponse = new Response(response.body, response);
+  // 添加允许跨域访问的响应头
+  modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
+  return modifiedResponse;
+}
+
