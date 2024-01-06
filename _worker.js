91280a565eeb6ec89041eb39ec3e1dfa99e850d8
Syz9031 <105356480+shuaiBi903@users.noreply.github.com>
2024-01-06 23:49:52 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ const app = express()
 const port = 9000
 
 app.use('/', createProxyMiddleware({
-  target: 'https://api.openai.com',
+  target: 'https://www.baidu.com',
   changeOrigin: true,
   onProxyReq: (proxyReq, req, res) => {
     // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
