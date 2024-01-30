9ae7c2045066a96cede4d64daf907c5e8dbc3fa7
Syz9031 <105356480+shuaiBi903@users.noreply.github.com>
2024-01-30 14:39:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "discord.com";
+    url.host = "https://discord.com/api/v9/channels";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
