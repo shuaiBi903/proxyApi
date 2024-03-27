0fd0681f64941b2a654829ab55efcaf62fb6d7e0
Syz9031 <105356480+shuaiBi903@users.noreply.github.com>
2024-03-27 10:55:17 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "api.githubcopilot.com";
+    url.host = "ab.chatgpt.com";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
